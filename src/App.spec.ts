be37640384d67d325b152e195d19611504af1ca1
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-09-12 17:29:16 +0200

    additional test

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { render } from "@testing-library/vue";
+import { render, fireEvent } from "@testing-library/vue";
 
 import App from "./App.vue";
 
@@ -10,5 +10,17 @@ describe("App", () => {
     expect(
       getByText("Click on the Vite and Vue logos to learn more")
     ).toBeDefined();
+  });
+
+  it("count increments correctly", async () => {
+    const { getAllByRole } = render(App);
+
+    const initialCountButton = getAllByRole("button").at(0) as HTMLElement;
+    expect(initialCountButton).toBeDefined();
+    expect(initialCountButton.innerHTML).toEqual("count is 0");
+
+    await fireEvent.click(initialCountButton);
+
+    expect(initialCountButton.innerHTML).toEqual("count is 1");
   });
 });
