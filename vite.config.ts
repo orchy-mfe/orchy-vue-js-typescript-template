a22008143e89be7cc06c54f301b988d21001949f
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-09-13 09:55:26 +0200

    cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
 /// <reference types="vitest" />
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
-import {visualizer} from 'rollup-plugin-visualizer'
-import qiankun from 'vite-plugin-qiankun'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
+import { visualizer } from "rollup-plugin-visualizer";
+import qiankun from "vite-plugin-qiankun";
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 8080
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
-export default defineConfig(({mode}) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     svgLoader(),
-    qiankun('orchy-vue-js-typescript-template', {useDevMode: true}),
+    qiankun("orchy-vue-js-typescript-template", { useDevMode: true }),
     visualizer(),
   ],
-  base: mode === 'development' ? `http://localhost:${port}/` : '/orchy-vue-js-typescript-template/',
-  server: {port},
+  base:
+    mode === "development"
+      ? `http://localhost:${port}/`
+      : "/orchy-vue-js-typescript-template/",
+  server: { port, cors: true },
   test: {
-    environment: 'happy-dom',
-    mockReset: true
+    environment: "happy-dom",
+    mockReset: true,
   },
-}))
+}));
