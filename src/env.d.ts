fa21882b987941d5cd93f7dde763a7753083f0c6
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-15 18:59:15 +0100

    fix: lint

--- src/env.d.ts
+++
@@ -1,7 +0,0 @@
-import { MicrofrontendProperties } from "@orchy-mfe/models";
-import "vite-plugin-qiankun/dist/helper";
-
-declare module "vite-plugin-qiankun/dist/helper" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface QiankunProps extends MicrofrontendProperties<any> {}
-}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import "./style.css";
 import AppVue from "./App.vue";
 
-import OrchyMicroFrontend from '@orchy-mfe/spa-adapter'
+import OrchyMicroFrontend from "@orchy-mfe/spa-adapter";
 import { MicroFrontendProperties } from "@orchy-mfe/models";
 
 export class VueMfeTypeScript extends OrchyMicroFrontend {
-  private app: App<Element> | undefined
+  private app: App<Element> | undefined;
 
   async mount(microFrontendProperties: MicroFrontendProperties) {
     this.app = createApp(AppVue);
@@ -25,7 +25,7 @@ export class VueMfeTypeScript extends OrchyMicroFrontend {
   async unmount() {
     if (this.app) {
       this.app.unmount();
-      if(this.app._container) this.app._container.innerHTML = "";
+      if (this.app._container) this.app._container.innerHTML = "";
     }
     this.app = undefined;
   }
