df3bc0929e3a578d2b8b1d2fdc9ee8553c6e229f
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-12-20 23:25:10 +0100

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,26 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import svgLoader from "vite-svg-loader";
 import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import svgLoader from "vite-svg-loader";
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    vue(),
-    svgLoader(),
-    visualizer(),
-    cssInjectedByJsPlugin()
-  ],
+  plugins: [vue(), svgLoader(), visualizer(), cssInjectedByJsPlugin()],
   base:
     mode === "development"
       ? `http://localhost:${port}/`
       : "/orchy-vue-js-typescript-template/",
   server: { port, cors: true },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
   test: {
     environment: "happy-dom",
     mockReset: true,
